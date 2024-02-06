8dacc228f2ef86bbd65608aa7b724eaac675da81
Dale Morris <dalemorris2021@gmail.com>
2024-02-06 12:40:24 -0500

    Added build system

--- src/main/java/edu/iu/App.java
+++ src/main/java/edu/iu/App.java
@@ -1,15 +1,15 @@
 package main.java.edu.iu;
 
-public class Observer implements Runnable {
+public class App implements Runnable {
     Agent[] agents;
     Obstacle[] obstacles;
     Goal goal;
     
     public static void main(String[] args) {
-        
+        (new App()).run();
     }
 
     public void run() {
-
+        System.out.println("Hello, World!");
     }
 }
